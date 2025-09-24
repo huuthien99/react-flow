122b1768ad29cfa5cfce4292b8337266c75ac811
ThienDh <dinhhuuthien99@gmail.com>
2025-09-24 13:18:48 +0700

    add-node

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,14 @@
 export const typeNodes = {
-  // START: { label: "Start" },
   WAIT: { label: "Wait" },
   HTTP_REQUEST: { label: "HTTP Request" },
   TERM_MEMORY: { label: "Term Memory" },
   LOG: { label: "Log" },
+  NEW_TAB: { label: "New Tab" },
+  OPEN_URL: { label: "Open URL" },
+  SWITCH_TAB: { label: "Switch Tab" },
+  CLOSE_TAB: { label: "Close Tab" },
+  RELOAD: { label: "Reload" },
+  GET_URL: { label: "Get URL" },
+  GET_CONTENT_HTML: { label: "Get Content HTML" },
+  SWITCH_IFRAME: { label: "Switch Iframe" },
 };
